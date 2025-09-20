3769e347e37b01dc784c11f1336a1af86155cae4
Lijo-Leon <lijoleon2002@gmail.com>
2025-09-20 21:45:31 +0530

    code updated

--- script.js
+++ script.js
@@ -1,44 +1,80 @@
 function addTask() {
-    task = taskInput.value
+    let task = taskInput.value;
     console.log(task);
 
     if (task === "") {
-        alert("Please add a List")
-    }
-
-    else {
-        let li = document.createElement("li")
-        
-        li.innerHTML = 
-        `<div class="flex items-center justify-left relative lg:w-[510px]">
-            <img src="https://i.pinimg.com/1200x/17/6b/21/176b21eae2b4ff4505d2f2f79110dc37.jpg" class="w-[45px]"  alt="">
-            ${task}
-            <div class="btn-div flex absolute right-0">
-                <button type="button" onclick="editTask(this)" class="px-[30px] py-[8px] ml-20 bg-green-500 text-white">Edit</button>
-                <button onclick="deleteTask(this)" class="px-[30px] py-[8px] ml-2 bg-red-500 text-white">Delete</button>
+        showAlert(`
+            <div class="bg-red-400 px-7 py-2 rounded-lg text-white">
+                <p><i class="fa-solid fa-triangle-exclamation mr-2"></i>Enter Task!</p>
             </div>
-        </div>`
+        `);
+    } else {
+        let li = document.createElement("li");
+
+        li.innerHTML = `
+            <div class="flex items-center justify-left relative lg:w-[510px]">
+                <img src="./tickbg.png" class="w-[45px]" alt="">
+                <span class="task-text ml-4">${task}</span>
+                <div class="btn-div flex absolute right-0">
+                    <button type="button" onclick="editTask(this)" class="px-[30px] py-[8px] ml-20 bg-green-500 text-white">Edit</button>
+                    <button onclick="deleteTask(this)" class="px-[30px] py-[8px] ml-2 bg-red-500 text-white">Delete</button>
+                </div>
+            </div>
+        `;
 
         taskList.appendChild(li);
-        taskInput.value = " "
+        taskInput.value = "";
+
+        showAlert(`
+            <div class="bg-green-500 px-7 py-2 rounded-lg text-white">
+                <p><i class="fa-solid fa-check mr-2"></i>Task Added</p>
+            </div>
+        `);
     }
 }
 
-function editTask(button){
-    console.log(button);
-    li = button.parentNode
-    console.log(li);
-    console.log(li.childNodes[2]);
-    currentText = li.childNodes[2].textContent
-    console.log(currentText);
-    newText = prompt("Edit Task ", currentText)
+function editTask(button) {
+    let li = button.closest("li");
+    let taskSpan = li.querySelector(".task-text");
 
-    if(newText){
-        li.childNodes[2].textContent = newText
+    let currentText = taskSpan ? taskSpan.textContent.trim() : "";
+
+    let newText = prompt("Edit Task", currentText);
+
+    if (newText && newText.trim() !== "") {
+        taskSpan.textContent = newText.trim();
+
+        showAlert(`
+            <div class="bg-green-500 px-7 py-2 rounded-lg text-white">
+                <p><i class="fa-solid fa-pen-to-square mr-2"></i>Task Edited</p>
+            </div>
+        `);
     }
 }
 
 function deleteTask(button) {
     let li = button.closest("li");
     li.remove();
+
+    showAlert(`
+        <div class="bg-red-500 px-7 py-2 rounded-lg text-white">
+            <p><i class="fa-solid fa-trash mr-2"></i>Task Deleted</p>
+        </div>
+    `);
+}
+
+
+let alertTimeout;
+
+function showAlert(content, duration = 4000) {
+    let alert = document.getElementById("alert");
+    alert.innerHTML = content;
+
+    if (alertTimeout) {
+        clearTimeout(alertTimeout);
+    }
+
+    alertTimeout = setTimeout(() => {
+        alert.innerHTML = "";
+    }, duration);
 }
